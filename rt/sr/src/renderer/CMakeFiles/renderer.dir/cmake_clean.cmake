d6623c12090252d90ba1a855af1bb34ed132f614
roman <piececakeof@gmail.com>
2021-01-05 21:30:42 +0200

    Adding software renderer from: https://github.com/loreglean/lantern

---
+++ rt/sr/src/renderer/CMakeFiles/renderer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderer.dir/LineClipper.cpp.o"
+  "CMakeFiles/renderer.dir/PolyClipper.cpp.o"
+  "CMakeFiles/renderer.dir/VertexProcessor.cpp.o"
+  "librenderer.a"
+  "librenderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
